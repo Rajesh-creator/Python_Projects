d865670822c2aae779aba7c3d525dc257fc8a270
Rajesh Barua <55019970+Rajesh-creator@users.noreply.github.com>
2023-02-21 23:54:35 +0600

    alarm_clock

---
+++ main.py
@@ -0,0 +1,28 @@
+from playsound import playsound
+import time
+
+CLEAR = "\033[2J"
+CLEAR_AND_RETURN = "\r\033[1G"
+
+
+def alarm(seconds):
+    time_elapsed = 0
+
+    print(CLEAR)
+    while time_elapsed < seconds:
+        time.sleep(1)
+        time_elapsed += 1
+
+        time_left = seconds - time_elapsed
+        minutes_left = time_left // 60
+        seconds_left = time_left % 60
+
+        print(f'{CLEAR_AND_RETURN}Alarm will sound in: {minutes_left:02d}:{seconds_left:02d}', end=' ')
+
+    playsound('Calm Down.mp3')
+
+
+minutes = int(input("How many minutes to wait: "))
+seconds = int(input("How many seconds to wait: "))
+total_seconds = minutes * 60 + seconds
+alarm(total_seconds)
